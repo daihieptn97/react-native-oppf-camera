008b99d739dd6fc35f2c6575e1b0f576569cdb4d
hiep <daihieptn97@gmail.com>
2021-06-29 16:14:09 +0700

    init

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,70 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import OppfCamera from 'react-native-oppf-camera';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+// import OppfCamera from 'react-native-oppf-camera';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+    // const [result, setResult] = React.useState<number | undefined>();
 
-  React.useEffect(() => {
-    OppfCamera.multiply(3, 7).then(setResult);
-  }, []);
+    React.useEffect(() => {
+        // OppfCamera.multiply(3, 7).then(setResult);
+    }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={{ marginHorizontal: 14 }}>
+                <TextInput
+                    style={styles.inputStyle}
+                    placeholder={'wifi name'} />
+                <TextInput
+                    style={styles.inputStyle}
+                    placeholder={'password'} />
+            </View>
+            <TouchableOpacity
+                style={styles.buttonStyle}>
+                <Text>SmartConfig</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
+    container: {
+        flex: 1,
+
+        justifyContent: 'center',
+    },
+    box: {
+        width: 60,
+        height: 60,
+        marginVertical: 20,
+    },
+    buttonStyle: {
+        backgroundColor: '#bebebe',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 50,
+        width: '60%',
+        borderRadius: 4,
+        alignSelf: 'center',
+
+        shadowColor: 'grey',
+
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+
+        elevation: 3,
+    }, inputStyle: {
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: '#bebebe',
+        marginBottom: 10,
+        paddingHorizontal: 14,
+    },
+
+
 });
